3123e41a4fe9ee618acde91f223d0386136dfc1e
Paul <paul18346@gmail.com>
2020-02-23 23:31:53 -0700

    Movimos a un monito en las cordenadas 0,0

---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+
+
+namespace Character_imput
+
+{
+
+    class Program
+
+    {
+
+        
+
+        //static int posX = 0;
+
+        //static int posY = 0;
+
+        static string lastPosPressed = "";
+
+
+
+        //arriba
+
+        static void PressUp() {
+
+            if(lastPosPressed == ""){
+
+               lastPosPressed = "up"; 
+
+            }
+
+            
+
+        }
+
+        static void ReleaseUp(){
+
+            if (lastPosPressed == "up"){
+
+             //mover hacia arriba
+
+              posY--;   
+
+              lastPosPressed = "";
+
+            }
+
+           
+
+        }
+
+
+
+        //abajo
+
+        static void PressRight() {
+
+            if(lastPosPressed == ""){
+
+               lastPosPressed = "Right"; 
+
+            }
+
+            
+
+        }
+
+
+
+         static void ReleaseRight()
+
+         {
+
+            if (lastPosPressed == "Right")
+
+            {
+
+              posX++;   
+
+              lastPosPressed = "";
+
+            }
+
+         }
+
+
+
+        static void PressDown() {
+
+            if(lastPosPressed == ""){
+
+               lastPosPressed = "down"; 
+
+            }
+
+            
+
+        }
+
+        static void ReleaseDown(){
+
+            if (lastPosPressed == "down"){
+
+             //mover hacia arriba
+
+              posY++;   
+
+              lastPosPressed = "";
+
+            }
+
+           
+
+        }
+
+        static void Main(string[] args)
+
+        {
+
+           // PressUp();
+
+           //ReleaseUp();
+
+
+
+            //PressRight();
+
+            //PressDown();
+
+            //ReleaseRight();
+
+
+
+            //PressRight();
+
+            //ReleaseRight();
+
+
+
+            for(int posX = 0; posX<=500; posX++)
+
+            for(int posY = 0; posY >= -500; posY--){
+
+
+
+            
+
+                Console.WriteLine(posX + "," + posY);
+
+            }
+
+        }
+
+    }
+
+}
